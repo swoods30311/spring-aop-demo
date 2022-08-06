fb8e3267c7dcbca0e880840b226ab7cf90925d55
Sandy Woods <sandywoods@Sandys-Air-2.attlocal.net>
2022-08-06 00:50:18 -0400

    Logging Aspect example

--- src/main/java/demo/ShoppingCart.java
+++ src/main/java/demo/ShoppingCart.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class ShoppingCart {
     public void checkout(){
+        //Logging
+        //Authentication & Authorization
+        //Sanitize the Data
         System.out.println("Checking out (method from shopping cart)");
     }
 }
